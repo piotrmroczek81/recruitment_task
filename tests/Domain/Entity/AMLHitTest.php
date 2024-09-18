347b86430612afd38d6f51870f8033afe0bc59a2
dev <dev@local>
2024-09-19 00:25:14 +0200

    AMLHitTest

--- tests/Domain/Entity/AMLHitTest.php
+++ tests/Domain/Entity/AMLHitTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Domain\Entity;
 
+use Domain\Entity\AMLHit;
 use Domain\Entity\Owner;
 use Domain\ValueObject\UUID;
 use PHPUnit\Framework\TestCase;
@@ -11,10 +12,14 @@ use PHPUnit\Framework\TestCase;
 class AMLHitTest extends TestCase {
     public function testOwnerCanHaveMultipleAMLHits(): void 
     {
-        $owner = new Owner(new UUID('550e8400-e29b-41d4-a716-446655440000'));
+        $owner = new Owner(new UUID('550e8400-e29b-41d4-a716-446655440000'));        
+        $this->assertCount(0, $owner->getHits()); 
 
 
+        $owner->addAMLHit(
+            new AMLHit(new UUID('550e8400-e29b-41d4-a716-446655440001'))
+        ); 
+        $this->assertCount(1, $owner->getHits()); 
     }
-
 
 }
