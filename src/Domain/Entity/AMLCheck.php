1e179385585d8267a3a1b92d2bb48e3a0465a28c
dev <dev@local>
2024-09-19 00:47:01 +0200

    AMLMonitorTest

---
+++ src/Domain/Entity/AMLCheck.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Entity;
+
+use Domain\ValueObject\UUID;
+
+class AMLCheck
+{
+    private UUID $id;
+
+    public function __construct(UUID $id)   {
+        $this->id = $id;
+    }        
+
+}
