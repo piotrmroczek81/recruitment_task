1bbcc88a95423bfd933e5117622a07e1aede6bff
dev <dev@local>
2024-09-19 00:16:11 +0200

    AMLHitTest

---
+++ tests/Domain/Entity/AMLHitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class AMLHitTest extends TestCase {
+    public function testOwnerCanHaveMultipleAMLHits(): void 
+    {
+
+    }
+
+
+}
