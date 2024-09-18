077e9f6eba859d7deb1b3fff34b2469b855d2b67
dev <dev@local>
2024-09-18 23:53:13 +0200

    CompanyTest

---
+++ src/Domain/Entity/Company.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Entity;
+
+use Domain\ValueObject\UUID;
+
+class Company
+{
+    private UUID $id;
+
+    private array $owners = [];
+    
+    public function __construct(UUID $id)    {
+        $this->id = $id;
+    }    
+
+    public function addOwner(Owner $owner): void
+    {
+        $this->owners[] = $owner;
+    }
+
+
+    public function getOwners(): array
+    {
+        return $this->owners;
+    }
+
+
+
+}
