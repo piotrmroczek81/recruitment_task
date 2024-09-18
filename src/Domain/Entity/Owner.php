0bb13307df26c7f9e5b47783f5d8de80f232c41e
piotrmroczek81 <141282346+piotrmroczek81@users.noreply.github.com>
2024-09-18 23:58:04 +0200

    testCompanyCanHaveMultipleOwners (#1)
    testCompanyCanHaveMultipleOwners

--- src/Domain/Entity/Owner.php
+++ src/Domain/Entity/Owner.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-
-
-
 namespace Domain\Entity;
 
 use Domain\ValueObject\UUID;
@@ -12,26 +9,8 @@ use Domain\ValueObject\UUID;
 class Owner
 {
     private UUID $id;
-    private string $firstName;
-    private string $lastName;
-    private array $companies = [];
-    private array $amlHits = [];
-
-    public function __construct(UUID $id, string $firstName, string $lastName)
-    {
+    
+    public function __construct(UUID $id)    {
         $this->id = $id;
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-    }
-
-    public function getTotalAMLHits() {
-        return $this->amlHits;
-    }
-
-
-    
-    public function accumulateAMLHit() {
-
-    }
-    
+    }    
 }
