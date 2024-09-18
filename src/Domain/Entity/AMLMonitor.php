707c9c75c03e961df55040e911fd0103badcc245
dev <dev@local>
2024-09-19 00:33:51 +0200

    AMLMonitorTest

---
+++ src/Domain/Entity/AMLMonitor.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Entity;
+
+use Domain\ValueObject\UUID;
+
+class AMLMonitor
+{
+    private UUID $id;
+
+
+    private array $amlChecks = [];
+
+    public function __construct(UUID $id)    {
+        $this->id = $id;
+    }        
+
+    
+    public function getAMLChecks(): array
+    {
+        return $this->amlChecks;
+    }
+
+}
