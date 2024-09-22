3b7117237690e05bb749cfe1ecab9c1790fc8f58
dev <dev@local>
2024-09-22 23:34:46 +0200

    testRunAMLMonitorForOwnerPerformsHisChecks

--- src/Domain/Entity/AMLCheck.php
+++ src/Domain/Entity/AMLCheck.php
@@ -9,10 +9,19 @@ use Domain\ValueObject\UUID;
 class AMLCheck
 {
     private UUID $id;
+    private Company $company;
 
-    public function __construct(UUID $id)   {
+    public function __construct(UUID $id, Company $company)   {
         $this->id = $id;
-    }        
+        $this->company = $company;
+    }
+    
+    public function getCompany(): Company {
+        return $this->company;
+    }
+
+
+    
 
     public function run(): void {
 
